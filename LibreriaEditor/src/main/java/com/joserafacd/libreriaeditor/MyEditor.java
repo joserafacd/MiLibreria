91d9d7915c6fe5d4e14fff1175b3b2ad68b5bb0f
joserafacd <joserafacd@hotmail.com>
2021-10-20 09:47:28 -0600

    Segunda Libreria

---
+++ LibreriaEditor/src/main/java/com/joserafacd/libreriaeditor/MyEditor.java
@@ -0,0 +1,9 @@
+package com.joserafacd.libreriaeditor;
+
+import android.util.Log;
+
+public class MyEditor {
+    public static void d(String TAG, String message){
+        Log.d(TAG, message);
+    }
+}
